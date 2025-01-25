b06d33d60cb933850d7e85a9dca5d06f22e59ea8
Ahn-seokjoo <tjrwn0716@naver.com>
2025-01-25 20:35:23 +0900

    typeorm + sqlite3 으로 간단한 db 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])],   /// 이걸 통해 repository가 자동으로 생성
 })
 export class UsersModule {}
