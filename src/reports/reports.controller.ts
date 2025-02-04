9de4a49ff9e1d12055ea32ef095a536b2a11c6cb
Ahn-seokjoo <tjrwn0716@naver.com>
2025-02-04 23:31:14 +0900

    currentUser middleware 추가, admin guard 추가, report 기능 추가

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGaurd } from '../guard/auth.guard';
@@ -6,6 +15,9 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { Serialize } from 'src/interceptor/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdiminGaurd } from 'src/guard/admin.guard';
+import { GetEstimateDto } from './dtos/get-esitmate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -16,5 +28,16 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdiminGaurd)
+  patchReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportService.createEstimate(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-esitmate.dto';
+import e from 'express';
 
 @Injectable()
 export class ReportsService {
@@ -13,5 +15,31 @@ export class ReportsService {
     report.user = user;
 
     return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+
+  async createEstimate(estimateDto: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make: estimateDto.make })
+      .andWhere('model = :model', { model: estimateDto.model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year: estimateDto.year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage: estimateDto.mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
